0cda4599d48e9a983d194ba7e49d8ce3b992dc06
franco948 <franco.palermo812@gmail.com>
2019-10-12 19:07:12 -0300

    Agregar tabla de actores y relacionarla con las peliculas

--- scripts/script.sql
+++ scripts/script.sql
@@ -8,6 +8,12 @@ CREATE TABLE genero (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE actor (
+  id int AUTO_INCREMENT,
+  nombre varchar(70),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE pelicula (
   id int AUTO_INCREMENT,
   titulo varchar(100),
@@ -19,6 +25,15 @@ CREATE TABLE pelicula (
   poster varchar(300),
   trama varchar(700),
   PRIMARY KEY (id)
+);
+
+CREATE TABLE actor_pelicula (
+  id int AUTO_INCREMENT,
+  actor_id int,
+  pelicula_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (actor_id) REFERENCES actor(id),
+  FOREIGN KEY (pelicula_id) REFERENCES pelicula(id)  
 );
 
 ALTER TABLE pelicula ADD COLUMN genero_id INT;
