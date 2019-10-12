294b9714d2968dfd5469ff93b760dc3db64c82e1
fpalermo <franco.palermo812@gmail.com>
2019-10-11 23:54:50 -0300

    Agregar controlador de peliculas y referenciarlo

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+var peliculasControlador = require('./controladores/peliculasControlador')
+
 var app = express();
 
 app.use(cors());
